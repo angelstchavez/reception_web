af16c8283da04f63edc0df812f0b2e6c1bfebe11
angelstchavez <angelstchavez@gmail.com>
2026-08-27 17:10:56 -0500

    feat: add theme toggle in the layout app

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -5,14 +5,16 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/private/utils/app-sidebar";
+import { ThemeToggle } from "@/components/theme/theme-toggle";
 
 export default function PrivateLayout({ children }: { children: ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 bg-card">
-          <SidebarTrigger className="-ml-1" />
+        <header className="flex h-16 shrink-0 items-center gap-1 border-b px-4 bg-card">
+          <SidebarTrigger />
+          <ThemeToggle />
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4">{children}</main>
       </SidebarInset>
